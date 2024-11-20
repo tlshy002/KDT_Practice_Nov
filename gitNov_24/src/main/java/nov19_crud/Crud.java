53797746f481fcf65cc26d195b37fa7972e73aa4
tlshy002 <tlshy002@gmail.com>
2024-11-20 21:41:52 +0900

    2024-11-20 commit01

--- gitNov_24/src/main/java/nov19_crud/Crud.java
+++ gitNov_24/src/main/java/nov19_crud/Crud.java
@@ -11,10 +11,12 @@ import nov14_notice.NoticeDTO;
 
 public class Crud { //매퍼의 쿼리를 호출
 
-	private final String MAPPER_NAME="noticeMapper"; //매퍼의 이름을 선언
+	private final String MAPPER_NAME="noticeMapper"; //매퍼의 이름을 선언. 환경설정파일의 namespace와 이름 매칭
 	
 	public int updateNotice(NoticeDTO dto) {
-		SqlSession ss = this.getSession(); int result = -1;
+		SqlSession ss = this.getSession(); 
+		int result = -1;
+		
 		try {
 			result = ss.update(MAPPER_NAME+".updateNotice", dto);
 			if(result > 0) ss.commit();
@@ -25,7 +27,8 @@ public class Crud { //매퍼의 쿼리를 호출
 		return result;
 	}
 	
-	public int deleteNotice(Integer dto) {
+	
+	public int deleteNotice(Integer dto) { //DAO의 기능
 		//매퍼파일의 쿼리를 호출하는 객체(SqlSession)를 생성한다
 		SqlSession ss = this.getSession();
 		int result = -1;
@@ -44,7 +47,8 @@ public class Crud { //매퍼의 쿼리를 호출
 	private SqlSession getSession() { 
 		//SqlSession을 생성하기 위해 DB정보가 필요하고, DB정보를 XML파일에 작성한다
 		// 따라서, SqlSession을 생성할 때 XML파일에서 DB정보를 불러온다.
-		String config = "mybatisConfig.xml"; 
+		
+		String config = "mybatisConfig.xml"; //환경설정 파일을 열기 위해 해당 파일이름 명시
 		InputStream is = null; //파일을 불러올때(열 때) 필요한 객체 선언
 		try {
 			is = Resources.getResourceAsStream(config); //XML파일을 연다
